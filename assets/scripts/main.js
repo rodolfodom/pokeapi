4267c4e8067cf44697451fa4d56489f6dbfffe03
rodolfodom <rodolfodominguez60@aragon.unam.mx>
2020-11-10 17:48:12 -0600

    I improved the script logic to avoid bugs

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -14,10 +14,12 @@ const baseURL = ()=>{
             console.log(myJson)
             document.getElementById("pokeinfo").style.display = "flex";
             document.getElementById("image").src = myJson.sprites.other["official-artwork"].front_default;
+            if(myJson.sprites.other["official-artwork"].front_default == null){
+                document.getElementById("image").src = myJson.sprites.front_default;
+            };
             arrValue.push(myJson.name);
             arrValue.push(myJson.id);
             arrValue.push(myJson.height);
-            
             let info = ["nombre", "ID", "altura"];
             for(let i = 0; i < info.length; i++){
                 document.getElementById(info[i]).innerHTML = arrValue[i];
